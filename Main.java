662f31407b4b0821e299ab525254fce390538d20
Vana8000 <163187974+Vana8000@users.noreply.github.com>
2024-03-14 22:01:11 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,31 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите строку:");
-        String str = input.nextLine();
+        String input = scanner.nextLine();
 
-        System.out.println("Введите подстроку:");
-        String substr = input.nextLine();
+        input = input.replace("кака", "вырезано цензурой");
+        input = input.replace("бяка", "вырезано цензурой");
 
-        int count = countSubstring(str, substr);
-
-        System.out.println("Подстрока встречается " + count + " раз");
-    }
-
-    public static int countSubstring(String str, String substr) {
-        int lastIndex = 0;
-        int count = 0;
-
-        while (lastIndex != -1) {
-            lastIndex = str.indexOf(substr, lastIndex);
-            if (lastIndex != -1) {
-                count++;
-                lastIndex += substr.length();
-            }
-        }
-
-        return count;
+        System.out.println("Результат замены: " + input);
     }
 }
