e69c3178406ff48088af43e89278a4022df81d33
Vana8000 <163187974+Vana8000@users.noreply.github.com>
2024-03-14 22:04:55 +0300

    Add files via upload
    Задание 4,2

---
+++ Mainn.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Mainn {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку:");
+        String input = scanner.nextLine();
+
+        input = input.replace("кака", "вырезано цензурой");
+        input = input.replace("бяка", "вырезано цензурой");
+
+        System.out.println("Результат замены: " + input);
+    }
+}
